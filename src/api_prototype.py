129c3eb848304bb9ca634093edb2dbbe087666fc
h593286 <593286@stud.hvl.no>
2024-01-31 19:01:22 +0100

    Updated prototypes
    Changed to get method, added dropdown of 30 cities

--- src/api_prototype.py
+++ src/api_prototype.py
@@ -1,18 +1,34 @@
+import json
+import requests
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Example data
-    data = {"message": "Hello, world!"}
-    return jsonify(data)
+# Load city data from JSON file
+with open('cities.json', 'r') as json_file:
+    city_data = json.load(json_file)
 
-@app.route('/api/data', methods=['POST'])
-def post_data():
-    new_data = request.json
-    # Process the new_data as needed
-    return jsonify({"message": "Data received", "received_data": new_data}), 201
+# Create a dictionary to easily look up cities by name
+city_lookup = {city['city']: city for city in city_data}
+
+@app.route('/get_firerisk_city', methods=['GET'])
+def get_city_data():
+    city_name = request.args.get('city')
+    if city_name and city_name in city_lookup:
+        city_info = city_lookup[city_name]
+        lat = city_info.get('lat')
+        lng = city_info.get('lng')
+        return jsonify({"message": "Data received", "city": city_name, "lat": lat, "lng": lng}), 200
+    else:
+        return jsonify({"message": "City not found"}), 404
+
+@app.route('/get_firerisk_coordinates', methods=['GET'])
+def get_coords_data():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    return jsonify({"message": "Data received", "lat": lat, "lng": lng}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001) #FOR LOCAL
--- src/prototype.py
+++ src/prototype.py
@@ -1,38 +1,32 @@
+import json
 import requests
-from flask import Flask, request, render_template_string
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def form_display():
-    return '''
-        <form method="post">
-            <input type="text" name="input1" placeholder="Input 1"/>
-            <input type="text" name="input2" placeholder="Input 2"/>
-            <input type="submit" value="Submit"/>
+    with open('cities.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    dropdown_html = '<select name="city">'
+    for city_info in data:
+        city_name = city_info['city']
+        dropdown_html += f'<option value="{city_name}">{city_name}</option>'
+    dropdown_html += '</select>'
+
+    return f'''
+        <form action="http://127.0.0.1:5001/get_firerisk_city" method="get">
+            {dropdown_html}
+            <input type="submit" value="Get FireRisk"/>
+        </form>
+        <form action="http://127.0.0.1:5001/get_firerisk_coordinates" method="get">
+            <input type="text" name="lat" placeholder="Latitude"/>
+            <input type="text" name="lng" placeholder="Longitude"/>
+            <input type="submit" value="Get FireRisk"/>
         </form>
     '''
-
-@app.route('/', methods=['POST'])
-def form_submit():
-    input1 = request.form['input1']
-    input2 = request.form['input2']
-
-    # API endpoint
-    api_url = 'http://127.0.0.1:5001/api/data'  # Local
-
-    coordinates = {
-        'field1': input1,
-        'field2': input2
-    }
-
-    response = requests.post(api_url, json=coordinates)
-
-    if response.status_code in [200, 201]:  # Handle both 200 OK and 201 Created
-        api_response = response.json()
-        return f'API Response: {api_response}'
-    else:
-        return f'Error calling API: Status code {response.status_code}'
 
 
 if __name__ == '__main__':
